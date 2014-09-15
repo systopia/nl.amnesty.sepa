4c31dfedb153a2aad0c18bb4ba422b6327333ed9
Xavier Dutoit <xavier@tttp.eu>
2014-09-15 23:16:40 +0200

    blank state

---
+++ api/v3/SepaMandate.php
@@ -0,0 +1,13 @@
+function civicrm_api3_sepa_mandate_migrate($params) {
+$params = array(
+  'sequential' => 1,
+  'pledge_status' => 'In Progress',
+  'option.limit' => 10,
+);
+$result = civicrm_api3('Pledge', 'get', $params);
+
+print_r($result);
+}
+
+
+
